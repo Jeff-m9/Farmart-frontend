9a6ee9e17aee479dbc8b7ef5b031d35bcae1596e
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-07-16 11:56:51 +0300

    Setup CartPage

---
+++ src/Pages/CartPage.jsx
@@ -0,0 +1,15 @@
+function CartPage() {
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      <p>Your selected animals will appear here.</p>
+      <ul>
+        <li>Golden Retriever - $800</li>
+        <li>Persian Cat - $600</li>
+      </ul>
+      <button>Proceed to Payment</button>
+    </div>
+  );
+}
+
+export default CartPage;
