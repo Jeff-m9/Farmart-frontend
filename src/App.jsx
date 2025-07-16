d683f9bb68b98e4f220cf2978669986e830c12d2
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-07-16 11:45:31 +0300

    Set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import CartPage from "./pages/CartPage";
+import PaymentPage from "./pages/PaymentPage";
+import AnimalDetails from "./pages/AnimalDetails";
+
 function App() {
   return (
-    <h1>Farmart</h1>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/dashboard" element={<BuyerDashboard />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+      <Route path="/animal/:id" element={<AnimalDetails />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
