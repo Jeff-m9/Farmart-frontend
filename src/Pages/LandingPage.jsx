13dd049fdb1fa7431ee1775f6d7e3585edba5faa
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-07-16 11:53:27 +0300

    set-up LandingPage

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Animal Marketplace</h1>
+      <p>Find your perfect pet or animal here!</p>
+    </div>
+  );
+}
+
+export default LandingPage;
