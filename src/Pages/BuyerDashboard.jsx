6d622d130442045674419a1ac3a912633909e76b
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-07-16 11:56:20 +0300

    SetUp BuyerDashboardPage

---
+++ src/Pages/BuyerDashboard.jsx
@@ -0,0 +1,15 @@
+function BuyerDashboard() {
+  return (
+    <div>
+      <h1>Buyer Dashboard</h1>
+      <p>Welcome back! Here are some featured animals for you:</p>
+      <ul>
+        <li>Dog - Golden Retriever</li>
+        <li>Cat - Persian</li>
+        <li>Bird - Parrot</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BuyerDashboard;
